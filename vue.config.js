9b7283aabfa56a08d5720cca30f72cbe70d58d96
Popping-z <943027274@qq.com>
2020-02-15 20:29:33 +0800

    2020.1.15修改

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+modules.exports = {
+  configureWebpack: {
+    resolve: {
+      alias: {
+        'assets': '@/assets',
+        'common': '@/common',
+        'components': '@/components',
+        'network': '@/network',
+        'views': '@/views',
+
+      }
+    }
+  }
+}
